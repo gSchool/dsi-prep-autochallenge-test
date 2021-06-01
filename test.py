1dbaed46036612de92d8253bca6962bf7fabca74
Lanceypantsy <34278955+Lanceypantsy@users.noreply.github.com>
2021-06-01 14:59:08 -0600

    Update test.py

--- test.py
+++ test.py
@@ -3,12 +3,11 @@ import matplotlib.testing.compare as test
 import numpy as np
 from PIL import Image
 import unittest
-import main
 
 class TestImageLikeness(unittest.TestCase):
     def test_image1_and_image2(self):
         img2 = Image.open('./images/plt2.png')
-        img3 = main.Image.open('./images/plt3.png')
+        img3 = Image.open('./images/plt3.png')
         img2_arr = np.asarray(img2)
         img3_arr = np.asarray(img3)
         rms_value = test.calculate_rms(img2_arr, img3_arr)
